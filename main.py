6834595338754b5ca25a848ba98a7c00961ff4ed
Beesy23 <Nathan.Beese@gmail.com>
2024-08-28 20:41:13 +0100

    updated main.py, player movement

--- main.py
+++ main.py
@@ -2,21 +2,36 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from constants import * # Import everything from constants.py
+from player import Player
 
 def main():
     pygame.get_init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    dt = 0 
+
+    while True: # Starts game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(screen,color=000)
-        pygame.display.flip()
+                return # Stops game loop if game window is closed
+        
+        screen.fill("black") # Fills game window with solid black colour
+        for obj in updateable: # Updates objects position
+            obj.update(dt)
 
-    '''print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")'''
+        for obj in drawable: # Draws objects on screen
+            obj.draw(screen)
+
+        pygame.display.flip() # Refreshes screen
+        dt = clock.tick(60)/1000 # This limits the framerate
+        
 
 if __name__ == "__main__":
     main()
