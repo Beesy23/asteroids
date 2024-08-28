783d2721ef0ff8e01c09f17be202a8883e58ceb5
Beesy23 <Nathan.Beese@gmail.com>
2024-08-28 21:59:01 +0100

    updated main.py, added asteroids, collision dectection for player and shoot

--- circleshape.py
+++ circleshape.py
@@ -18,3 +18,8 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collides(self, other_circle):
+        if self.position.distance_to(other_circle.position) < self.radius + other_circle.radius:
+            return True
+        return False
--- main.py
+++ main.py
@@ -2,17 +2,29 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import * # Import everything from constants.py
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.get_init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+    Shot.containers = (shots, updateable, drawable)
+
+    asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     dt = 0 
@@ -22,9 +34,15 @@ def main():
             if event.type == pygame.QUIT:
                 return # Stops game loop if game window is closed
         
-        screen.fill("black") # Fills game window with solid black colour
         for obj in updateable: # Updates objects position
             obj.update(dt)
+
+        for ast in asteroids:
+            if player.collides(ast):
+                #print("Game over!")
+                sys.exit("Game over!")
+        
+        screen.fill("black") # Fills game window with solid black colour
 
         for obj in drawable: # Draws objects on screen
             obj.draw(screen)
